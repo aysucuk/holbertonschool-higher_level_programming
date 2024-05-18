093377a6660d36ce53e44c96d81ce3cbbd8c0281
aysucuk <aysusadixova@gmail.com>
2024-05-18 18:37:22 +0000

    Fizz Buzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(0, 101):
+        if i % 3:
+            print("Fizz", end=" ")
+        elif i % 5:
+            print("Buzz", end=" ")
+        elif i % 15:
+            print("FizzBuzz", end=" ")
+        else:
+            print(i, end=" ")
