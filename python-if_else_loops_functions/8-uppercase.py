505d7936fcf8fc84cb383c35489e2f6236e2bbce
aysucuk <aysusadixova@gmail.com>
2024-05-18 17:50:40 +0000

    To uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord[i] > 96 and ord[i] < 123:
+            ord[i] = ord[i] - 32
+            print(ord[i])
+        else:
+            print(ord[i])
