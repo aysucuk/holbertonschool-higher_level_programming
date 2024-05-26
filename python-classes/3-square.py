ee7a44986e2ee6a01b59614c01508ab0e6a9a6fb
aysucuk <aysusadixova@gmail.com>
2024-05-26 16:36:19 +0000

    error fixed

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -11,3 +11,9 @@ class Square:
             raise TypeError("size must be an integer")
         elif self.__size < 0:
             raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Public instance method that returns the
+        current square area.
+        """
+        return self.__size ** 2
